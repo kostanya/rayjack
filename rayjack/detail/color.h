ebc0a8e7b70b45c0b3b622cd1717cfd1b699f11c
kostanya <keskinfurkan01@gmail.com>
2023-10-26 11:30:08 +0300

    change pixel color values from linear space to gamma space before writing to the file

--- rayjack/detail/color.h
+++ rayjack/detail/color.h
@@ -3,6 +3,8 @@
 #include "interval.h"
 #include "utils.h"
 
+inline float linearToGamma(float linearComponent) { return std::sqrt(linearComponent); }
+
 void writeColor(uint8_t* imageData, int& idx, color pixelColor, const int samplesPerPixel) {
     float r = pixelColor.r;
     float g = pixelColor.g;
@@ -14,10 +16,14 @@ void writeColor(uint8_t* imageData, int& idx, color pixelColor, const int sample
     g *= scale;
     b *= scale;
 
+    // Apply the linear to gamma transform
+    r = linearToGamma(r);
+    g = linearToGamma(g);
+    b = linearToGamma(b);
+
     // Write the translated [0,255] value of each color component.
     static const Interval intensity(0.000f, 0.999f);
     imageData[idx++] = static_cast<uint8_t>(256 * intensity.clamp(r));
     imageData[idx++] = static_cast<uint8_t>(256 * intensity.clamp(g));
     imageData[idx++] = static_cast<uint8_t>(256 * intensity.clamp(b));
 }
-
