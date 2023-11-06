cab8c63f71054b5c891acfc3cfc4fa5d857ae5a7
sinemKocoglu <sinemkocoglu560@gmail.com>
2023-11-06 23:51:22 +0300

    integrated catch2 framework

---
+++ tests/test_ray.cpp
@@ -0,0 +1,21 @@
+#include <catch2/catch_test_macros.hpp>
+#include "ray.h"
+
+TEST_CASE("Test Ray Class") {
+    SECTION("Test Ray Constructor") {
+        Ray ray({0, 0, 0}, {1, 0, 0});
+
+        REQUIRE(ray.origin() == point3(0, 0, 0));
+        REQUIRE(ray.direction() == vec3(1, 0, 0));
+    }
+
+    SECTION("Test Ray 'at' method") {
+        Ray ray({0, 0, 0}, {1, 0, 0});
+
+        point3 point_at_t = ray.at(3);
+
+        point3 expected_point = {3, 0, 0};
+
+        REQUIRE(point_at_t == expected_point);
+    }
+}
